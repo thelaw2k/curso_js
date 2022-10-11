b8c7f7de0a12af0a4538c3c7108d77082a9dd7d6
thelaw2k <thelaw2k@gmail.com>
2022-10-11 14:34:55 -0300

    tarea clase numero 1

---
+++ 01/js/script.js
@@ -0,0 +1,10 @@
+let numero = 5;
+
+function consultar(){
+    let numero2 = prompt("Ingrese un numero");
+    alert(numero * numero2);
+
+    let texto1 = prompt("Ingrese un nombre");
+    let texto2 = prompt("Ingrese un apellido");
+    console.log("Hola " + texto1 + " " + texto2)
+}
